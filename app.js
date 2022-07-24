ec666e291cde7f9fba169cfffad482a00888c9b9
BaekJinhyeok <109152602+BaekJinhyeok@users.noreply.github.com>
2022-07-24 23:47:46 +0900

    Learn JavaScript Conditions

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-const a = 5;
-const b = 3;
+const age = parseInt(prompt("How old are you?"));
 
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
+if (isNaN(age) || age < 0) {
+  console.log("Please write a real positive number");
+} else if (age < 19) {
+  console.log("You are too young");
+} else if (age >= 19 && age <= 50) {
+  console.log("You can drink alcohol");
+} else if (age === 100) {
+  console.log("wow you are wise");
+} else if (age > 50) {
+  console.log("Recommend you not to drink alcohol");
+}
